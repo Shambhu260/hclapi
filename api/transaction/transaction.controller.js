5576f354c8050d19887c8c847c5a1abf0fc9b198
Shambhu260 <shambhu2892@gmail.com>
2024-05-11 17:23:17 +0530

    api changes

--- api/transaction/transaction.controller.js
+++ api/transaction/transaction.controller.js
@@ -1,4 +1,3 @@
-
 const transactionModel = require("./transaction.model");
 // const jwt = require('jsonwebtoken');
 exports.register = async function (req,res){
@@ -32,6 +31,13 @@ exports.getTransactionById = async function (req,res){
     }
 }
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(bodyParser.json());
 
 // Sample user model and storage (in-memory for simplicity)
 const users = [];
